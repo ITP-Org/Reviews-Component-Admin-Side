d80efcd2284a9de5ca63d6ee816dc8d85349f626
CharukaIllangasinghe <charuka0716@gmail.com>
2024-10-11 01:09:34 +0530

    Add files via upload

---
+++ RFooter.jsx
@@ -0,0 +1,22 @@
+import "./RFooter.css";
+
+function RFooter() {
+    return (
+      <>
+      <div className = "report-footer">
+        <div className="report-address">
+          <h4>Saumika Senanayake</h4>
+          <h4>123/150/A</h4>
+          <h4>Ward place</h4>
+          <h4>Colombo</h4>
+        </div>
+        <div>
+          <h3 className = "report-copyright">2024 &copy; Saumika Senanayake</h3>
+        </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default RFooter;
+  
